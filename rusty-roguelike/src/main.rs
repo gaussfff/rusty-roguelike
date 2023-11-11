d7f91a00f33364d77430c3c3ce5472442fae9fd5
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2023-11-11 19:27:42 +0200

    add tilemap, add lib

--- rusty-roguelike/src/main.rs
+++ rusty-roguelike/src/main.rs
@@ -1,3 +1,16 @@
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ rusty-roguelike/src/map.rs
@@ -0,0 +1,19 @@
+use crate::NUM_TILES;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
