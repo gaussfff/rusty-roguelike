eebd51111fdc4531b6fba8f299ce2646fe5c68d9
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2023-11-12 08:23:32 +0200

    add map rendering

--- rusty-roguelike/src/main.rs
+++ rusty-roguelike/src/main.rs
@@ -11,6 +11,30 @@ use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
-fn main() {
-    println!("Hello, world!");
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            map: Map::new()
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
 }
