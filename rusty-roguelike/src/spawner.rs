f895ebd84ce29ed3f0c11d8ca63061cef82f0fdf
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2023-11-16 19:15:19 +0200

    added monster spawner, added collisions detector

--- rusty-roguelike/src/spawner.rs
+++ rusty-roguelike/src/spawner.rs
@@ -8,3 +8,21 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
 
     ecs.push((Player, pos, render));
 }
+
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    let render = Render {
+        color: ColorPair::new(WHITE, BLACK),
+        glyph: match rng.range(0, 4) {
+            0 => to_cp437('E'),
+            1 => to_cp437('O'),
+            2 => to_cp437('o'),
+            _ => to_cp437('g'),
+        }
+    };
+
+    ecs.push((Enemy, pos, render));
+}
