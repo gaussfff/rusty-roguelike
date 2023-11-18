5456f89f6a8f854924cdbc1db4b292dd59402877
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2023-11-18 15:36:17 +0200

    added combat system

--- rusty-roguelike/src/systems/player_input.rs
+++ rusty-roguelike/src/systems/player_input.rs
@@ -5,12 +5,14 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState
 ) {
     if let Some(key) = key {
+        let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
@@ -19,16 +21,35 @@ pub fn player_input(
             _ => Point::new(0, 0)
         };
 
+        let (player_entity, destination) = players
+            .iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
         if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            });
+            let mut hit_something = false;
+            enemies
+                .iter(ecs)
+                .filter(|(_, pos)| {
+                    ** pos == destination
+                })
+                .for_each(|(entity, _)| {
+                    hit_something = true;
+
+                    commands.push(((), WantsToAttack {
+                        attacker: player_entity,
+                        victim: *entity
+                    }));
+                });
+
+            if !hit_something {
+                commands.push(((), WantsToMove {
+                    entity: player_entity,
+                    destination
+                }));
+            }
         }
+
+        *turn_state = TurnState::PlayerTurn;
     }
 }
