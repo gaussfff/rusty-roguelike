58d88f64363831b9bc553bebe8f748081a996462
Bohdan Sokolovskyi <sokol.chemist@gmail.com>
2023-11-23 23:34:51 +0200

    added using items

--- rusty-roguelike/src/systems/hud.rs
+++ rusty-roguelike/src/systems/hud.rs
@@ -20,8 +20,23 @@ pub fn hud(ecs: &SubWorld) {
         .unwrap();
 
     let mut item_query = <(&Item, &Name, &Carried)>::query();
-    let mut y = 3;
 
+    draw_batch.target(2);
+    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
+    draw_batch.bar_horizontal(
+        Point::zero(),
+        SCREEN_WIDTH * 2,
+        player_health.current,
+        player_health.max,
+        ColorPair::new(RED, BLACK)
+    );
+    draw_batch.print_color_centered(
+        0,
+        format!(" Health: {} / {}", player_health.current, player_health.max),
+        ColorPair::new(WHITE, RED)
+    );
+
+    let mut y = 3;
     item_query
         .iter(ecs)
         .filter(|(_, _, carried)| carried.0 == player)
@@ -38,19 +53,5 @@ pub fn hud(ecs: &SubWorld) {
         draw_batch.print_color(Point::new(3, 2), "Items carried", ColorPair::new(YELLOW, BLACK));
     }
 
-    draw_batch.target(2);
-    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
-    draw_batch.bar_horizontal(
-        Point::zero(),
-        SCREEN_WIDTH * 2,
-        player_health.current,
-        player_health.max,
-        ColorPair::new(RED, BLACK)
-    );
-    draw_batch.print_color_centered(
-        0,
-        format!(" Health: {} / {}", player_health.current, player_health.max),
-        ColorPair::new(WHITE, RED)
-    );
     draw_batch.submit(10000).expect("Batch error");
 }
